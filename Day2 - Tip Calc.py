318064db592ef7cb966e927ed7f2526811d5747e
Matheus Pulis <114489604+mtspulis21@users.noreply.github.com>
2023-05-17 01:11:09 -0300

    Add files via upload

---
+++ Day2 - Tip Calc.py
@@ -0,0 +1,6 @@
+print('Welcome to the Tip Calculator.')
+bill = float(input('What was the total bill?\n$'))
+tip = int(input('What percentage tip would you like to give? 10, 12 or 15?\n$'))
+split = int(input('How many people to split the bill?\n$'))
+payment = round((bill + (bill *(tip / 100))) / split,2)
+print(f'Each person will pay {payment}')
