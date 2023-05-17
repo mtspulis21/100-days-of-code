a801f9fb00806aef0e453b767151106e48f6be2c
Matheus Pulis <114489604+mtspulis21@users.noreply.github.com>
2023-05-17 01:00:57 -0300

    Add files via upload

---
+++ Day1 - BandName.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator')
+city = input('Please enter the city where you grew up:\n ')
+pet = input('Please enter the name of your first pet:\n ')
+print(f'This is your new band name: {city} {pet}')
+
